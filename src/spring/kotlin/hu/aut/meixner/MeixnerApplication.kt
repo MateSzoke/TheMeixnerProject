5973f575a21650b6bfba539976f4628d84de72f0
Szőke Máté <szoke.mate@autsoft.hu>
2020-03-19 20:49:56 +0100

    Add Swagger UI to project

--- src/spring/kotlin/hu/aut/meixner/MeixnerApplication.kt
+++ src/spring/kotlin/hu/aut/meixner/MeixnerApplication.kt
@@ -2,8 +2,10 @@ package hu.aut.meixner
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class MeixnerApplication
 
 fun main(args: Array<String>) {
