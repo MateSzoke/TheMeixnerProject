f94359ee29cfa272964f3974803c3ccf66de5ba4
Szőke Máté <szoke.mate@autsoft.hu>
2020-03-23 14:12:07 +0100

    Add datasource config to hikari

--- src/spring/kotlin/hu/aut/meixner/DatabaseConfig.kt
+++ src/spring/kotlin/hu/aut/meixner/DatabaseConfig.kt
@@ -3,6 +3,7 @@ package hu.aut.meixner
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import javax.sql.DataSource
@@ -13,9 +14,13 @@ class DatabaseConfig {
     private val dbUrl: String? = null
 
     @Bean
-    fun dataSource(): DataSource {
+    fun dataSource(dataSourceProperties: DataSourceProperties): DataSource {
         val config = HikariConfig()
         config.jdbcUrl = dbUrl
+        config.driverClassName = dataSourceProperties.driverClassName
+        config.jdbcUrl = dataSourceProperties.url
+        config.username = dataSourceProperties.username
+        config.password = dataSourceProperties.password
         return HikariDataSource(config)
     }
 }
