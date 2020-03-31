45fc727c03b0c8174d5a6b1997747dcf36465739
Szőke Máté <szoke.mate@autsoft.hu>
2020-04-01 01:54:55 +0200

    Try disable database config

--- src/spring/kotlin/hu/aut/meixner/DatabaseConfig.kt
+++ src/spring/kotlin/hu/aut/meixner/DatabaseConfig.kt
@@ -1,33 +1,27 @@
 package hu.aut.meixner
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import javax.sql.DataSource
-
-@Configuration
-class DatabaseConfig {
-    @Value("\${spring.datasource.url}")
-    private val dbUrl: String? = null
-
-    @Value("\${spring.datasource.driver-class-name}")
-    private val driverClassName: String? = null
-
-    @Value("\${spring.datasource.username}")
-    private val username: String? = null
-
-    @Value("\${spring.datasource.password}")
-    private val password: String? = null
-
-    @Bean
-    fun dataSource(): DataSource {
-        val config = HikariConfig()
-        config.jdbcUrl = dbUrl
-        config.driverClassName = driverClassName
-        config.username = username
-        config.password = password
-        return HikariDataSource(config)
-    }
-}
+//
+//@Configuration
+//class DatabaseConfig {
+//    @Value("\${spring.datasource.url}")
+//    private val dbUrl: String? = null
+//
+//    @Value("\${spring.datasource.driver-class-name}")
+//    private val driverClassName: String? = null
+//
+//    @Value("\${spring.datasource.username}")
+//    private val username: String? = null
+//
+//    @Value("\${spring.datasource.password}")
+//    private val password: String? = null
+//
+//    @Bean
+//    fun dataSource(): DataSource {
+//        val config = HikariConfig()
+//        config.jdbcUrl = dbUrl
+//        config.driverClassName = driverClassName
+//        config.username = username
+//        config.password = password
+//        return HikariDataSource(config)
+//    }
+//}
