63cbd3ae4713cf712c58566778c4a3868b82f290
Szőke Máté <szoke.mate@autsoft.hu>
2020-04-20 15:16:54 +0200

    Add missing @Entity annotation to media item entity

--- src/spring/kotlin/hu/aut/meixner/domain/task/MediaItemEntity.kt
+++ src/spring/kotlin/hu/aut/meixner/domain/task/MediaItemEntity.kt
@@ -1,9 +1,11 @@
 package hu.aut.meixner.domain.task
 
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
+@Entity
 data class MediaItemEntity(
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long = 0,
