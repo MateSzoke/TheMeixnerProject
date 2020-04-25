4d8b0460545ccd931331df134a1871d8b450be4c
Szőke Máté <szoke.mate@autsoft.hu>
2020-04-25 18:18:44 +0200

    Create grouping and sorting task request, response and entity classes

--- src/spring/kotlin/hu/aut/meixner/dto/task/complex/GroupingAndSortingResponse.kt
+++ src/spring/kotlin/hu/aut/meixner/dto/task/complex/GroupingAndSortingResponse.kt
@@ -2,28 +2,18 @@ package hu.aut.meixner.dto.task.complex
 
 import hu.aut.meixner.dto.SubjectEnum
 import hu.aut.meixner.dto.task.common.TaskTypeEnum
-import hu.aut.meixner.dto.task.easy.SortingResponse
+import hu.aut.meixner.dto.task.easy.GroupResponse
 import java.time.OffsetDateTime
 
 class GroupingAndSortingResponse(
-        override val id: Long,
-        override val type: TaskTypeEnum = TaskTypeEnum.GroupingAndSorting,
-        override val title: String,
-        override val owner: String,
-        override val subject: SubjectEnum,
-        override val difficulty: Int,
-        override val recommendedMinClass: Int,
-        override val recommendedMaxClass: Int,
-        override val lastModified: OffsetDateTime
-) : SortingResponse(
-        elements = mutableListOf(),
-        id = id,
-        type = type,
-        lastModified = lastModified,
-        title = title,
-        owner = owner,
-        subject = subject,
-        recommendedMinClass = recommendedMinClass,
-        recommendedMaxClass = recommendedMaxClass,
-        difficulty = difficulty
+        val id: Long,
+        val groups: List<GroupResponse>,
+        val type: TaskTypeEnum = TaskTypeEnum.GroupingAndSorting,
+        val title: String,
+        val owner: String,
+        val subject: SubjectEnum,
+        val difficulty: Int,
+        val recommendedMinClass: Int,
+        val recommendedMaxClass: Int,
+        val lastModified: OffsetDateTime
 )
