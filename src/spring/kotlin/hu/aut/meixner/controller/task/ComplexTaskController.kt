8ccf300e035cee64a02dbd80e80264b46f63133d
Szőke Máté <szoke.mate@autsoft.hu>
2020-04-25 18:27:21 +0200

    Create complex task controller

---
+++ src/spring/kotlin/hu/aut/meixner/controller/task/ComplexTaskController.kt
@@ -0,0 +1,14 @@
+package hu.aut.meixner.controller.task
+
+import io.swagger.annotations.Api
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Api(tags = ["The complex tasks"], description = "Add and update Grouping and sorting, Sentence completion and grouping, Sentence completion and sorting, Sentence creation and grouping, Sentence creation and sorting, Sorting and grouping")
+@RestController
+@RequestMapping("/tasks")
+class ComplexTaskController(
+
+) {
+
+}
--- src/spring/kotlin/hu/aut/meixner/mapping/ComplexTaskEntityDTO.kt
+++ src/spring/kotlin/hu/aut/meixner/mapping/ComplexTaskEntityDTO.kt
@@ -1,2 +1,37 @@
 package hu.aut.meixner.mapping
 
+import hu.aut.meixner.dto.task.complex.GroupingAndSortingRequest
+import hu.aut.meixner.dto.task.complex.GroupingAndSortingResponse
+import hu.aut.meixner.entity.task.complex.GroupingAndSortingEntity
+import hu.aut.meixner.entity.task.easy.GroupElementEntity
+import hu.aut.meixner.entity.task.easy.GroupingEntity
+import java.time.OffsetDateTime
+
+//region GroupingAndSorting
+fun GroupingAndSortingRequest.toEntity(owner: String, groups: List<GroupElementEntity>): GroupingEntity {
+    return GroupingEntity(
+            groups = groups,
+            title = title,
+            difficulty = difficulty,
+            owner = owner,
+            subject = subject,
+            recommendedMinClass = recommendedMinClass,
+            recommendedMaxClass = recommendedMaxClass,
+            lastModified = OffsetDateTime.now()
+    )
+}
+
+fun GroupingAndSortingEntity.toDomainModel(): GroupingAndSortingResponse {
+    return GroupingAndSortingResponse(
+            id = id,
+            lastModified = lastModified,
+            difficulty = difficulty,
+            title = title,
+            owner = owner,
+            subject = subject,
+            recommendedMinClass = recommendedMinClass,
+            recommendedMaxClass = recommendedMaxClass,
+            groups = groups.map { it.toDomainModel() }
+    )
+}
+//endregion
