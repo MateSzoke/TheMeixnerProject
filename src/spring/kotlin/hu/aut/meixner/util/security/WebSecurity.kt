5ced251ebc2b11e2c33e1c621aa8663d7fbb7d5c
FBalazsBme <FBme1993%>
2020-04-26 22:03:06 +0200

    Add logout to backend.

--- src/spring/kotlin/hu/aut/meixner/util/security/WebSecurity.kt
+++ src/spring/kotlin/hu/aut/meixner/util/security/WebSecurity.kt
@@ -5,6 +5,7 @@ import hu.aut.meixner.util.security.SecurityConstants.AUTH_WHITELIST
 import hu.aut.meixner.util.security.SecurityConstants.REGISTER_URL
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -33,6 +35,10 @@ class WebSecurity(
                 .addFilter(JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(JWTAuthorizationFilter(authenticationManager())) // this disables session creation on Spring Security
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .logout()
+                .permitAll()
+                .logoutSuccessHandler(HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
     }
 
     public override fun configure(auth: AuthenticationManagerBuilder) {
