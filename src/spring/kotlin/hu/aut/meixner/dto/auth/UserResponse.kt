bbd3a5a5f86468c3c092be14d4ca9e33421d146b
Szőke Máté <szoke.mate@autsoft.hu>
2020-04-28 23:18:20 +0200

    Add get current user endpoint and fix global currentUser variable

--- src/spring/kotlin/hu/aut/meixner/dto/auth/UserResponse.kt
+++ src/spring/kotlin/hu/aut/meixner/dto/auth/UserResponse.kt
@@ -1,6 +1,9 @@
 package hu.aut.meixner.dto.auth
 
+import hu.aut.meixner.entity.auth.UserRoleEnum
+
 data class UserResponse(
         val id: Long,
-        val username: String
+        val username: String,
+        val role: UserRoleEnum
 )
