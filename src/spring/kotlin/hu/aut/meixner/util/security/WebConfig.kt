d33cc5b19983360db2d374e05b946217ba97ee64
FBalazsBme <FBme1993%>
2020-05-11 19:17:32 +0200

    Cors allowed from backend

---
+++ src/spring/kotlin/hu/aut/meixner/util/security/WebConfig.kt
@@ -0,0 +1,18 @@
+package hu.aut.meixner.util.security
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+class ApplicationConfig : WebMvcConfigurerAdapter() {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        // Can just allow `methods` that you need.
+        registry.addMapping("/**").allowedMethods("PUT", "GET", "DELETE", "OPTIONS", "PATCH", "POST")
+    }
+}
