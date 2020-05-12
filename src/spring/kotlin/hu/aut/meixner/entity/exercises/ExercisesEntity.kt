88816e860bde6d7304f88a7d3fe5316f9d87b1cd
Szőke Máté <szoke.mate@autsoft.hu>
2020-05-12 18:10:06 +0200

    Create my exercises endpoint, add owner to exercise

--- src/spring/kotlin/hu/aut/meixner/entity/exercises/ExercisesEntity.kt
+++ src/spring/kotlin/hu/aut/meixner/entity/exercises/ExercisesEntity.kt
@@ -8,6 +8,7 @@ data class ExercisesEntity(
         val id: Long = 0,
         val name: String = "",
         val comment: String = "",
+        val owner: String = "",
         @ElementCollection(targetClass = Long::class)
         val taskIds: MutableList<Long> = mutableListOf()
 )
