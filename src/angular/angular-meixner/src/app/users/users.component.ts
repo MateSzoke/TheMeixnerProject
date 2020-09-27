bb3e6a511999c1e773bd47ed3e6d18f050d4f05e
Szőke Máté <szoke.mate@autsoft.hu>
2020-09-27 23:14:16 +0200

    Add user list to users page

--- src/angular/angular-meixner/src/app/users/users.component.ts
+++ src/angular/angular-meixner/src/app/users/users.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
-import {AccountService, UserRequest} from "../../swagger-api";
+import {AccountService, UserRequest, UserResponse} from "../../swagger-api";
 import {ConvertEnum} from "../model/ConvertEnum";
+import RoleEnum = UserRequest.RoleEnum;
 
 @Component({
   selector: 'app-users',
@@ -12,17 +13,24 @@ import {ConvertEnum} from "../model/ConvertEnum";
 export class UsersComponent implements OnInit {
 
   public roles: Array<string> = ['Admin', 'Diák'];
-  private role : number = -1;
+  users: Array<UserResponse>;
+  public wrongUser = false;
   username: string = null;
   password: string = null;
   errorMessage = 'Invalid Credentials';
-  public wrongUser  = false;
+  private role: number = -1;
 
   ngOnInit() {
     this.roles = new Array<string>();
-    for(let i in UserRequest.RoleEnum) {
+    for (let i in UserRequest.RoleEnum) {
       this.roles.push(ConvertEnum.convertRole(i));
     }
+    //mock data, TODO get it from server
+    this.users = [
+      {"id": 1, "role": RoleEnum.STUDENT, "username": "test student"},
+      {"id": 2, "role": RoleEnum.STUDENT, "username": "Kiss Béla"},
+      {"id": 3, "role": RoleEnum.STUDENT, "username": "Kovács Ilona"},
+      {"id": 4, "role": RoleEnum.STUDENT, "username": "Árpád Gizella"}]
   }
 
   constructor(private router: Router,
@@ -31,37 +39,49 @@ export class UsersComponent implements OnInit {
   }
 
   register() {
-    if(this.username == null || this.password == null || this.role == -1) {
+    if (this.username == null || this.password == null || this.role == -1) {
       alert("Kérem adja meg az összes adatot!");
       return;
     } else {
 
       let j = 0;
-      let roleEnum : string ;
-      for(let i in UserRequest.RoleEnum) {
-        if(j == this.role) {
+      let roleEnum: string;
+      for (let i in UserRequest.RoleEnum) {
+        if (j == this.role) {
           roleEnum = i;
         }
         j++;
       }
-      this.accountService.registerUsingPOST({username: this.username,
-      password: this.password, role: roleEnum} as UserRequest).subscribe(
-            data => {
-              alert("Felhasználó hozzáadva!");
-            },
-            error => {
+      this.accountService.registerUsingPOST({
+        username: this.username,
+        password: this.password, role: roleEnum
+      } as UserRequest).subscribe(
+        data => {
+          alert("Felhasználó hozzáadva!");
+        },
+        error => {
 
-            },
-            () => {
+        },
+        () => {
 
-            }
-          );
+        }
+      );
     }
-
-
   }
 
   public roleChange(input) {
     this.role = input.value;
+  }
+
+  openMyExercises(userId: number) {
+    // TODO open exercises dialog and bind for the given user
+  }
+
+  openUserResults(userId: number) {
+    // TODO open results of the given user
+  }
+
+  deleteUser(userId: number) {
+    // TODO delete user
   }
 }
