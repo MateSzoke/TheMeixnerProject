362fcc90c852b34ec4d8076dd31dae1ac32fb551
Szőke Máté <szoke.mate@autsoft.hu>
2020-10-07 15:27:11 +0200

    Bind change student class level to frontend

--- src/angular/angular-meixner/src/app/app.module.ts
+++ src/angular/angular-meixner/src/app/app.module.ts
@@ -36,7 +36,7 @@ import {GroupingSortingComponent} from './task_types/grouping-sorting/grouping-s
 import {SentencecompletionGroupingComponent} from './task_types/sentencecompletion-grouping/sentencecompletion-grouping.component';
 import {SentencecompletionSortingComponent} from './task_types/sentencecompletion-sorting/sentencecompletion-sorting.component';
 import {SentencecreationGroupingComponent} from './task_types/sentencecreation-grouping/sentencecreation-grouping.component';
-import {SentenceCreationSortingComponent} from './task_types/sentencecreation-sorting/sentence-creation-sorting.component';
+import {SentencecreationSortingComponent} from './task_types/sentencecreation-sorting/sentencecreation-sorting.component';
 import {SortingGroupingComponent} from './task_types/sorting-grouping/sorting-grouping.component';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
@@ -66,7 +66,7 @@ import {ExerciseListComponent} from './users/excercise-list/exercise-list.compon
     SentencecompletionGroupingComponent,
     SentencecompletionSortingComponent,
     SentencecreationGroupingComponent,
-    SentenceCreationSortingComponent,
+    SentencecreationSortingComponent,
     SortingGroupingComponent,
     ExerciseTaskListComponent,
     UserResultsComponent,
