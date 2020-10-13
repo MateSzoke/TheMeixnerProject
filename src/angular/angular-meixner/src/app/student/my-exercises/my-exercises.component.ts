2ae8203627aed8cdf8f00406b6facad8e75f1411
Szőke Máté <szoke.mate@autsoft.hu>
2020-10-14 00:44:30 +0200

    Create my exercises component UI

--- src/angular/angular-meixner/src/app/student/my-exercises/my-exercises.component.ts
+++ src/angular/angular-meixner/src/app/student/my-exercises/my-exercises.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {AssignedExercise} from "../../../swagger-api/model/assignedExercise";
+import {AssignService} from "../../../swagger-api";
 
 @Component({
   selector: 'app-my-exercises',
@@ -7,10 +9,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MyExercisesComponent implements OnInit {
 
-  constructor() {
+  loaded: boolean = false
+  exercises: Array<AssignedExercise> = new Array<AssignedExercise>()
+
+  constructor(
+    private assignService: AssignService
+  ) {
   }
 
   ngOnInit(): void {
+    this.assignService.getMyExercisesUsingGET().subscribe(exercises => {
+      this.exercises = exercises
+      this.loaded = true
+    })
   }
 
+  solveExercise(exercise: AssignedExercise) {
+    this.assignService.getTasksByExerciseIdUsingGET(exercise.id).subscribe(tasks => {
+      console.log(tasks)
+    })
+  }
 }
