8028f98754562379b16b091b36e6b1183c59b8fd
Szőke Máté <szoke.mate@autsoft.hu>
2020-10-19 15:45:12 +0200

    Fix pairing evaluate

--- src/spring/kotlin/hu/aut/meixner/service/result/EasyTaskEvaluationService.kt
+++ src/spring/kotlin/hu/aut/meixner/service/result/EasyTaskEvaluationService.kt
@@ -30,7 +30,7 @@ class EasyTaskEvaluationService(
                 if (requestPair.pair.equalsResultMediaItems(resultPair.pair)) match++
             }
         }
-        return saveTaskRequest(student, taskId, taskResult, calculateResultPercentage(taskRequest.pairs, taskResult.pairs.filter { it.pair.isNotEmpty() }, match))
+        return saveTaskRequest(student, taskId, taskResult, calculateResultPercentage(taskRequest.pairs.filter { it.pair.isNotEmpty() }, taskResult.pairs, match))
     }
 
     fun evaluateGrouping(taskId: Long, taskRequest: GroupingTaskRequest): TaskResultResponse? {
