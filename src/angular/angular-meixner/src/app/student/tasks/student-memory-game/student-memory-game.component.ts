b23a8c00a546ec2a1b89b8cbabbe8cdba47e0899
Szőke Máté <szoke.mate@autsoft.hu>
2020-10-26 01:45:09 +0100

    Change memory game student task

--- src/angular/angular-meixner/src/app/student/tasks/student-memory-game/student-memory-game.component.ts
+++ src/angular/angular-meixner/src/app/student/tasks/student-memory-game/student-memory-game.component.ts
@@ -1,8 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {AssignService, EvaluateService} from "../../../../swagger-api";
+import {
+  AssignService,
+  EvaluateService,
+  MediaItemResponse,
+  MemoryGameResponse,
+  TaskService
+} from "../../../../swagger-api";
 import {MatDialog} from "@angular/material/dialog";
-import {CdkDragDrop, transferArrayItem} from "@angular/cdk/drag-drop";
 import {MyExercisesComponent} from "../../my-exercises/my-exercises.component";
 import {MemoryGameTask} from "../../../../swagger-api/model/memoryGameTask";
 import {MemoryGameTaskRequest} from "../../../../swagger-api/model/memoryGameTaskRequest";
@@ -17,41 +22,38 @@ export class StudentMemoryGameComponent implements OnInit {
   loaded: boolean = false
   startedExerciseId: number = null
   taskId: number = null
-  memory: MemoryGameTask = null
+  memory: MemoryGameResponse = null
+  elements: Array<MemoryUI> = new Array<MemoryUI>()
   memoryRequest: MemoryGameTaskRequest = null
-  currentResult: Array<Boolean> = new Array<Boolean>()
+  firstElement: MemoryUI = null
+  secondElement: MemoryUI = null
+  correctPairs: number = 0
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private evaluateService: EvaluateService,
     private dialog: MatDialog,
-    private assignService: AssignService
+    private assignService: AssignService,
+    private taskService: TaskService
   ) {
   }
 
   ngOnInit() {
     this.taskId = Number.parseInt(this.route.snapshot.paramMap.get("taskId"))
     this.startedExerciseId = Number.parseInt(this.route.snapshot.paramMap.get("startedExerciseId"))
-    this.assignService.getStudentTaskByIdUsingGET(this.taskId).subscribe(task => {
-      this.memory = task as MemoryGameTask
-      let pairs = []
-      for (let i = 0; i < (this.memory.elements.length / 2); i++) {
-        pairs.push({pair: []})
-      }
+    this.taskService.getTaskByIdUsingGET(this.taskId).subscribe(task => {
+      this.memory = task as MemoryGameResponse
       this.memoryRequest = {
-        pairs: pairs
+        pairs: []
       }
-      this.loaded = true
+      this.assignService.getStudentTaskByIdUsingGET(this.taskId).subscribe(task => {
+        let memoryTask = task as MemoryGameTask
+        memoryTask.elements.forEach(mediaItem => this.elements.push(new MemoryUI(mediaItem)))
+        this.loaded = true
+      })
     })
-  }
 
-  drop(event: CdkDragDrop<Array<any>, any>) {
-    if (event.container.data.length >= 2) return
-    transferArrayItem(event.previousContainer.data,
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex);
   }
 
   evaluateTask() {
@@ -59,7 +61,6 @@ export class StudentMemoryGameComponent implements OnInit {
       console.log(response.taskResult)
       this.loaded = false
       if (response.taskResult.taskResult == undefined) {
-        this.currentResult = response.taskResult.currentResult
         this.memoryRequest.attempts = response.taskResult.attempts
       } else {
         this.dialog.open(MemoryGameResultComponent, {
@@ -72,5 +73,66 @@ export class StudentMemoryGameComponent implements OnInit {
       }
       this.loaded = true
     })
+  }
+
+  elementClicked(element: MemoryUI, index: number) {
+    if (this.secondElement != null || element.clicked) {
+      return
+    }
+    this.elements[index].clicked = true
+    if (this.firstElement == null) {
+      this.firstElement = this.elements[index]
+    } else {
+      this.secondElement = this.elements[index]
+    }
+    if (this.secondElement != null) {
+      let correct = false
+      this.memory.pairs.forEach(pair => {
+        if (pair.pair[0].content == this.firstElement.mediaItem.content && pair.pair[1].content == this.secondElement.mediaItem.content
+          || pair.pair[1].content == this.firstElement.mediaItem.content && pair.pair[0].content == this.secondElement.mediaItem.content) {
+          this.elements[this.elements.indexOf(this.firstElement)].correct = true
+          this.elements[this.elements.indexOf(this.secondElement)].correct = true
+          correct = true
+          this.memoryRequest.pairs.push(pair)
+        }
+      })
+      if (correct) {
+        this.firstElement = null
+        this.secondElement = null
+      }
+    }
+  }
+
+  getSuccess(index: number): Boolean {
+    if (this.secondElement != null)
+      return this.elements[index].correct && this.elements[index].clicked
+    else
+      return false
+  }
+
+  getFail(index: number): Boolean {
+    if (this.secondElement != null)
+      return !this.elements[index].correct && this.elements[index].clicked
+    else
+      return false
+  }
+
+  reset() {
+    this.elements = this.elements.map(element => new MemoryUI(element.mediaItem))
+    this.firstElement = null
+    this.secondElement = null
+    this.memoryRequest.pairs = []
+  }
+}
+
+class MemoryUI {
+  mediaItem: MediaItemResponse
+  correct: boolean
+  clicked: boolean
+
+  constructor(mediaItem: MediaItemResponse) {
+    this.mediaItem = mediaItem
+    this.correct = false
+    this.clicked = false
   }
 }
