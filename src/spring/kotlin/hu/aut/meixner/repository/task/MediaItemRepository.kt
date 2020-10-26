231f12911c816b2222c7b3b0dc9293565e1e86c6
Szőke Máté <szoke.mate@autsoft.hu>
2020-10-26 17:56:20 +0100

    Make MediaItemService to Transactional

--- src/spring/kotlin/hu/aut/meixner/repository/task/MediaItemRepository.kt
+++ src/spring/kotlin/hu/aut/meixner/repository/task/MediaItemRepository.kt
@@ -2,7 +2,5 @@ package hu.aut.meixner.repository.task
 
 import hu.aut.meixner.entity.task.MediaItemEntity
 import org.springframework.data.repository.CrudRepository
-import org.springframework.transaction.annotation.Transactional
 
-@Transactional
 interface MediaItemRepository : CrudRepository<MediaItemEntity, Long>
