337ce85ec37ef9984db634200329dc3df87838b4
Szőke Máté <szoke.mate@autsoft.hu>
2020-11-03 17:43:20 +0100

    Fix student-sentence-completion-and-sorting.component

--- src/angular/angular-meixner/src/app/student/tasks/student-sentence-creation-and-grouping/student-sentence-creation-and-grouping.component.ts
+++ src/angular/angular-meixner/src/app/student/tasks/student-sentence-creation-and-grouping/student-sentence-creation-and-grouping.component.ts
@@ -1,4 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {AssignService, EvaluateService, Sentence} from "../../../../swagger-api";
+import {MatDialog} from "@angular/material/dialog";
+import {CdkDragDrop, transferArrayItem} from "@angular/cdk/drag-drop";
+import {SentenceCreationResultComponent} from "../result/sentence-creation-result/sentence-creation-result.component";
+import {SentenceCreationAndGroupingTask} from "../../../../swagger-api/model/sentenceCreationAndGroupingTask";
+import {SentenceCreationAndGroupingTaskRequest} from "../../../../swagger-api/model/sentenceCreationAndGroupingTaskRequest";
 
 @Component({
   selector: 'app-student-sentence-creation-and-grouping',
@@ -6,11 +13,81 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['../student-tasks.scss']
 })
 export class StudentSentenceCreationAndGroupingComponent implements OnInit {
+  loaded: boolean = false
+  startedExerciseId: number = null
+  taskId: number = null
+  sentenceCreation: SentenceCreationAndGroupingTask = null
+  taskRequest: SentenceCreationAndGroupingTaskRequest = null
+  currentResult: Array<Boolean> = new Array<Boolean>()
+  groupingAvailable: boolean = false
+  sentences: Array<Sentence> = new Array<Sentence>()
 
-  constructor() {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private evaluateService: EvaluateService,
+    private dialog: MatDialog,
+    private assignService: AssignService
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.taskId = Number.parseInt(this.route.snapshot.paramMap.get("taskId"))
+    this.startedExerciseId = Number.parseInt(this.route.snapshot.paramMap.get("startedExerciseId"))
+    this.assignService.getStudentTaskByIdUsingGET(this.taskId).subscribe(task => {
+      this.sentenceCreation = task as SentenceCreationAndGroupingTask
+      this.taskRequest = {
+        sentenceGroups: this.sentenceCreation.groupTitles.map(title => ({groupTitle: title, sentences: []}))
+      }
+      this.sentences = this.sentenceCreation.sentenceTitles.map(title => ({sentenceTitle: title, parts: []}))
+      this.loaded = true
+    })
   }
 
+  setGroupingAvailable(available: boolean) {
+    this.groupingAvailable = available
+  }
+
+  getSuccess(index: number): Boolean {
+    if (this.currentResult.length != 0)
+      return this.currentResult[index]
+    else
+      return false
+  }
+
+  getFail(index: number): Boolean {
+    if (this.currentResult.length != 0)
+      return !this.currentResult[index]
+    else
+      return false
+  }
+
+  drop(event: CdkDragDrop<Array<any>, any>) {
+    transferArrayItem(event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex);
+    console.log(this.taskRequest)
+  }
+
+  evaluateTask() {
+    this.evaluateService.evaluateSentenceCreationAndGroupingUsingPOST(this.startedExerciseId, this.taskId, this.taskRequest).subscribe(response => {
+      console.log(response.taskResult)
+      this.loaded = false
+      if (response.taskResult.taskResult == undefined) {
+        this.currentResult = response.taskResult.currentResult
+        this.taskRequest.attempts = response.taskResult.attempts
+      } else {
+        this.dialog.open(SentenceCreationResultComponent, {
+          data: {startedExercise: response},
+          disableClose: true
+        })
+      }
+      this.loaded = true
+    })
+  }
+
+  dropSentence(event: CdkDragDrop<Array<Sentence>, any>) {
+
+  }
 }
